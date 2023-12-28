6ecafc19ddf17ab3e867172752d2f6e9463e5718
TharlyRodrigues <tharly.portifolio@gmail.com>
2023-12-28 09:45:26 -0300

    update

--- script.js
+++ script.js
@@ -23,4 +23,30 @@ const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
 
 const appleOrangesJuice = fruitProcessor(2, 5);
-console.log(appleOrangesJuice);*/
+console.log(appleOrangesJuice);
+
+const num = Number("23");
+
+function tenis(nike, adidas) {
+  const compraTenis = `voce comprou ${nike} Nike e ${adidas} Adidas`;
+  return compraTenis;
+}
+
+const novosTenis = tenis(5, 4);
+console.log(novosTenis);
+
+// function  declaration
+function calcAge1(birthYeah) {
+  return 2026 - birthYeah;
+}
+const age1 = calcAge1(1995);
+
+
+// function Expression
+
+const calcAge2 = function (birthYeah) {
+  return 2037 - birthYeah;
+};
+const age2 = calcAge2(1995);
+
+console.log(age1, age1);*/
