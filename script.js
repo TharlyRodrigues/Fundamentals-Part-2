aa5eeaeed8df3edcfaa41532a2460bb62ad24414
TharlyRodrigues <tharly.portifolio@gmail.com>
2023-12-28 09:03:37 -0300

    Fundamentals-Part-2

---
+++ script.js
@@ -0,0 +1,26 @@
+/*let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+
+function logger() {
+  console.log("My name is Tharly");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangesJuice = fruitProcessor(2, 5);
+console.log(appleOrangesJuice);*/
