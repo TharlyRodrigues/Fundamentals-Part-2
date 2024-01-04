328d0c31a309108023956669ec75735245e52584
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-04 12:58:37 -0300

    update

--- script.js
+++ script.js
@@ -283,4 +283,51 @@ console.log(jonas.getSummary());
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`lifting weights repetition ${rep}🏋️`);
 }
+
+
+// array
+
+const jonas = [
+  "jonas",
+  "schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["michael", "peter", "steven"],
+  true,
+];
+
+const type = [];
+
+for (let i = 0; i < jonas.length; i++) {
+  console.log(jonas[i], typeof jonas[i]);
+
+  // type[i] = typeof jonas[i];
+
+  type.push(typeof jonas[i]);
+}
+
+console.log(type);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+
+console.log(ages);
+
+console.log("---ONLY STRINGS---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "string") continue;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log("---ONLY with NUMBER---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === "number") break;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
 */
